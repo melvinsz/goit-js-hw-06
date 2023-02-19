97b21199ee810f6f4191f35241c6646d81e4cbf1
Stanislav <Stas.zaliiski@gmail.com>
2023-02-19 17:16:30 +0100

    6 задач

--- js/task-06.js
+++ js/task-06.js
@@ -1,4 +1,5 @@
-// Напиши скрипт, який під час втрати фокусу на інпуті (подія blur), перевіряє його вміст щодо правильної кількості введених символів.
+// Напиши скрипт, який під час втрати фокусу на інпуті (подія blur), перевіряє його вміст
+// щодо правильної кількості введених символів.
 
 // <input
 //   type="text"
@@ -8,8 +9,10 @@
 // />
 
 // Яка кількість символів повинна бути в інпуті, зазначається в його атрибуті data-length.
-// Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
-// Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
+// Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна
+// кількість - червоним.
+// Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні
+// файли завдання.
 
 // #validation-input {
 //   border: 3px solid #bdbdbd;
@@ -22,3 +25,23 @@
 // #validation-input.invalid {
 //   border-color: #f44336;
 // }
+
+const inputRef = document.querySelector("#validation-input");
+function checkLength(event) {
+  if (event.currentTarget.value.length === Number(inputRef.dataset.length)) {
+    validRef();
+  } else {
+    invalidRef();
+  }
+}
+
+function validRef() {
+  inputRef.classList.remove("invalid");
+  inputRef.classList.add("valid");
+}
+function invalidRef() {
+  inputRef.classList.remove("valid");
+  inputRef.classList.add("invalid");
+}
+
+inputRef.addEventListener("blur", checkLength);
